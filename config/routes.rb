7d825ce3267cbf308bb05087fee3a872d3d03739
Ken Tran <loschorts@gmail.com>
2017-02-23 19:09:05 -0800

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :bookings
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "bookings#index"
 end
+
